03fea34505ca93ca2bded0d374149f6276a72e6a
Olja <olja.ivkovic97@gmail.com>
2018-02-06 18:28:10 +0100

    Add animation for nav, scrolling widget and animation

---
+++ sajt.js
@@ -0,0 +1,53 @@
+function createFromTemplate() {
+  var temp = '<button class="fas fa-caret-up" id="strelica"></buttons>';
+  var element = $(temp);
+  $("body").append(element);
+}
+
+$(document).ready(function() {
+  var sendvic = $("#sandwich");
+  var iks = $("#x");
+  var glMeni = $(".gl-meni");
+  var aniSpeed = 500;
+  createFromTemplate();
+
+  var isScrollingAnimate = false;
+  $("#strelica").click(function() {
+    isScrollingAnimate = true;
+    $("html").animate({scrollTop: 0}, 200, function() {
+      isScrollingAnimate = false;
+    });
+    $("#strelica").fadeOut();
+  });
+
+  $(document).scroll(function() {
+    if(isScrollingAnimate) return;
+    if($("html").scrollTop() == 0) {
+      $("#strelica").fadeOut();
+    }
+    $("#strelica").show();
+  });
+
+  sendvic.click(function() {
+    /*glMeni.css({
+      display: "block"
+    });*/
+    glMeni.animate({
+      right:0
+    }, aniSpeed , function() {
+      sendvic.hide();
+    });
+
+  });
+
+  iks.click(function() {
+    /*glMeni.css({
+      display:"none"
+    });*/
+
+    sendvic.show();
+    glMeni.animate({
+      right:"-25%"
+    }, aniSpeed);
+  })
+});
